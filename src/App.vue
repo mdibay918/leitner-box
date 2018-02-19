f7c28d994cc1f0a3cee352756aec77141939e735
coding6122 <coding6122@gmail.com>
2018-02-19 04:16:40 -0800

    flash-card init.
     + Card component
     + Card transition with onclick event handler

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app" class="container is-fluid">
+		<router-view/>
+	</div>
+</template>
+<script>
+export default {
+	name: 'App'
+}
+</script>
+<style>
+	#app {
+		padding-top: 60px;
+	}
+</style>
---
+++ src/views/LitnerReview.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="columns is-centered" @click="flip">
+		<div class="column is-one-third prev">
+			<i class="fas fa-angle-left"></i>
+		</div>
+		<div class="column">
+			<transition name="component-fade" mode="out-in">
+				<!-- <component v-bind:is="view"></component> -->
+				<div key="3" v-if="showQuestion">
+					<card header="Question" :content="question"/>
+				</div>
+				<div key="1" v-if="!showQuestion">
+					<card header="Answer" :content="answer"/>
+				</div>
+			</transition>
+		</div>
+		<div class="column is-one-third next">
+			<i class="fas fa-angle-right"></i>
+		</div>
+	</div> 
+</template>
+<script>
+import card from '@/components/Card'
+export default {
+	name: 'leitnerbox', 
+	components: {
+		card: card
+	},
+	data() {
+		return {
+			question: 'What\'s your name?',
+			answer: 'Foo!',
+			showQuestion: true, 
+			view: 'v-a'
+		}
+	}, 
+	methods: {
+		flip() {
+			this.showQuestion =  !this.showQuestion;
+		}
+	}
+}
+</script>
+<style>
+.prev {
+	text-align: right;
+	padding-top: 50px;
+}
+.next {
+	padding-top: 50px;
+}
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
